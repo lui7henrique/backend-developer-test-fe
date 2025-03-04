ea4d68442f35f2890bf2ccc3e56d34592d3555dd
Luiz Henrique <7henrique18@gmail.com>
2025-03-04 13:54:40 -0300

    refactor: improve type safety and parameter handling in doctor page component

--- src/app/doctor/[id]/page.tsx
+++ src/app/doctor/[id]/page.tsx
@@ -7,10 +7,14 @@ import { redirect } from "next/navigation";
 import { DoctorCalendar } from "./_doctor-calendar";
 import { DoctorBookedSlots } from "./_doctor-schedule";
 
-export default async function DoctorPage({
-	params,
-}: { params: { id: string } }) {
-	const { doctor } = await getDoctorById(params.id);
+type DoctorPageProps = {
+	params: Promise<{ id: string }>;
+};
+
+export default async function DoctorPage({ params }: DoctorPageProps) {
+	const { id } = await params;
+
+	const { doctor } = await getDoctorById(id);
 
 	if (!doctor) {
 		return redirect("/");
@@ -45,13 +49,13 @@ export default async function DoctorPage({
 
 					<TabsContent value="available">
 						<BlurFade delay={0.4}>
-							<DoctorCalendar id={params.id} />
+							<DoctorCalendar id={id} />
 						</BlurFade>
 					</TabsContent>
 
 					<TabsContent value="booked">
 						<BlurFade delay={0.4}>
-							<DoctorBookedSlots id={params.id} />
+							<DoctorBookedSlots id={id} />
 						</BlurFade>
 					</TabsContent>
 				</Tabs>
